5cf285d3ac506e8872dd7fc22a463977de7b7a44
Ricardo Caicedo <c-rcaicedo@neogov.net>
2026-07-16 09:26:12 -0400

    feat(api): add create user command validator

---
+++ api/MeetMind.Application/Users/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+
+namespace MeetMind.Application.Users.Commands.CreateUser;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Email format is not valid");
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .MaximumLength(100).WithMessage("First name cannot exceed 100 characters");
+
+        RuleFor(x => x.LastName)
+            .NotEmpty().WithMessage("Last name is required")
+            .MaximumLength(100).WithMessage("Last name cannot exceed 100 characters");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required");
+    }
+}
