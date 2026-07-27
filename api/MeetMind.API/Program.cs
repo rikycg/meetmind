99a76907cb5f3b6223ebc539f7641fb6b0069d74
Ricardo Caicedo <c-rcaicedo@neogov.net>
2026-07-27 09:31:38 -0400

    feat(api): add api docs

--- api/MeetMind.API/Program.cs
+++ api/MeetMind.API/Program.cs
@@ -1,12 +1,22 @@
 using MeetMind.API.Middlewares;
 using MeetMind.Application;
 using MeetMind.Infrastructure;
+using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MeetMind API",
+        Version = "v1",
+        Description = "AI-powered meeting assistant API"
+    });
+});
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(connectionString!);
 
@@ -15,7 +25,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "MeetMind API v1");
+        options.RoutePrefix = "docs";
+    });
 }
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
